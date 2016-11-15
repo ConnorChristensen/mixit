acd56d20d6d6cfbc56bd83f577807573322e8e24
Connor Christensen <chriconn@oregonstate.edu>
2016-11-14 19:51:02 -0800

    addition of a script to compile scss and addition of general use scss files

---
+++ globalSCSScompiler.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#compile landing page
+scss scss/main.scss main.css
+
+#compile catalog
+scss catalog/catalog.scss catalog/catalog.css
+
+#compile register
+scss register/register.scss register/register.css
+
+#compile login
+scss login/login.scss login/login.css
