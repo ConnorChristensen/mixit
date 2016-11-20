f2d6bbae5820452e84a1cbbce7164340b45092ed
Connor Christensen <chriconn@oregonstate.edu>
2016-11-20 12:02:48 -0800

    creation of login page by reformatting code from register page

---
+++ login/login.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name='viewport' content='width=device-width'>
+	<title>Enter Info</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script type="text/javascript" src="../register/register.js"></script>
+	<link rel="stylesheet" href="login.css">
+</head>
+
+<body>
+   <a href="../index.php" class="linkContainer">
+        <div>Back</div>
+    </a>
+    
+	<?php
+        //conects the database
+        include('databaseConnect.php');
+        //if the table doesn't exist, then create it
+        include('createTable.php');
+        //check that the data is valid and post it if it is
+        include('postData.php');
+    ?>
+    
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <h1>Login</h1>
+
+        <label for="userName">User Name</label>
+        <input id="userName" type="text" name="userName" class="textBox required">
+        <span class="error" id="userNameError">
+            <?php echo $user["error"]; ?>
+        </span>
+
+        <label for="password">Password</label>
+        <input id="password" type="password" name="password" class="textBox required">
+        <span class="error" id="passwordError">
+            <?php echo $password["error"]; ?>
+        </span>
+        
+        <div style="text-align: center">
+            <input type="submit" value="submit" id="submit">
+        </div>
+    </form>
+    <?php
+        mysqli_close($conn);
+    ?>
+</body>
+
+</html>
