11b9a91b60a6d7c304e7d2151848db2f058b6d1e
Connor Christensen <chriconn@oregonstate.edu>
2016-11-26 01:15:55 -0800

    creation of results page

---
+++ results/results.php
@@ -0,0 +1,21 @@
+<?php
+    // Start the session
+    session_start();
+?>
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Results</title>
+    <link rel="stylesheet" href="results.css">
+</head>
+
+<body>
+    <a href="../search/search.php" class="back">
+       <div>Back</div>
+    </a>
+</body>
+
+</html>
