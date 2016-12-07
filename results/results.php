d30e4d9ff2df0dab6b7c6844d668a0bd25f1d52b
Robert Rosenberger <rosenber@oregonstate.edu>
2016-12-06 16:50:37 -0800

    made results actually use the stuff of the queries

--- results/results.php
+++ results/results.php
@@ -29,39 +29,14 @@
         <div>Back</div>
     </a>
     <div class="resultsContainer">
-        <div class="drinkCard">
-            <div class="imgContainer">
-                <img src="" alt="">
-            </div>
-            <h2>Australian Gold</h2>
-            <h4>Ingredients</h4>
-            <ul>
-                <li>1oz rum</li>
-                <li>1oz mango liqueur</li>
-                <li>1oz galliano</li>
-                <li>garnish 1 small pineapple wedge</li>
-            </ul>
-            <h4>Description</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas accusamus quod minima et necessitatibus voluptatibus, voluptate a natus, explicabo blanditiis atque at delectus distinctio aut adipisci, doloremque suscipit excepturi deleniti.</p>
-            <h4>Instructions</h4>
-            <p>Build over ice</p>
-        </div>
-        <div class="drinkCard">
-            <div class="imgContainer">
-                <img src="" alt="">
-            </div>
-            <h2>Fizzy Rush</h2>
-            <h4>Ingredients</h4>
-            <ul>
-                <li>1 tsp white creme de menthe</li>
-                <li>1/3oz apricot brandy</li>
-                <li>1oz champagne</li>
-            </ul>
-            <h4>Description</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas accusamus quod minima et necessitatibus voluptatibus, voluptate a natus, explicabo blanditiis atque at delectus distinctio aut adipisci, doloremque suscipit excepturi deleniti.</p>
-            <h4>Instructions</h4>
-            <p>Pour in order</p>
-        </div>
+        
+        <?php
+        
+        generateHTMLOfQuery();
+        
+        ?>
+        
+        
     </div>
 </body>
 
