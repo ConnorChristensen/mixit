4ec50b47fdef3adc8ea9927b68f808bdc471b6df
Connor Christensen <chriconn@oregonstate.edu>
2016-12-08 18:19:46 -0800

    added some interface changes to search

--- search/search.js
+++ search/search.js
@@ -44,6 +44,47 @@ $(document).ready(function() {
         }
     });
     
+<<<<<<< Updated upstream
     
     
+=======
+    //set a var that will contain the number of boxes
+    var numBoxes = 10;
+    
+    //set a var that will contain the length of the list container
+    var foreverWidth = parseInt($(".list").css("width"));
+    
+    $(".rightClick").click(function() {
+        //reset numBoxes to zero to get a proper count
+//        numBoxes = 0;
+        //get the width of the scroll container
+        var containerWidth = parseInt($(".scrollContainer").css("width"));
+        
+        //count the number of items in the container
+//        $(this).siblings(".scrollContainer .item").each(function() {
+//            numBoxes++;
+//        });
+
+        
+        if (parseInt($(this).siblings(".scrollContainer").css("left")) > -(foreverWidth+containerWidth)) {
+            $(this).siblings(".scrollContainer").css("left", "-="+foreverWidth/(numBoxes/4)+"px");
+        } else {
+            $(this).siblings(".scrollContainer").css("left", -(numBoxes*100)+"px");
+        }
+    });
+    $(".leftClick").click(function() {
+//        numBoxes = 0;
+        
+        //count the number of items in the container
+//        $(this).siblings(".scrollContainer .item").each(function() {
+//            numBoxes++;
+//        });
+        
+        if (parseInt($(this).siblings(".scrollContainer").css("left")) < 0) {
+            $(this).siblings(".scrollContainer").css("left", "+="+foreverWidth/(numBoxes/4)+"px");
+        } else {
+            $(this).siblings(".scrollContainer").css("left", "0px");
+        }
+    });
+>>>>>>> Stashed changes
 });
