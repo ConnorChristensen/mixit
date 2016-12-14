647db456fad1f72b47bf71e9c94810efc470421b
Connor Christensen <chriconn@oregonstate.edu>
2016-12-14 10:33:12 -0800

    added in the connection information for Connor's phpMyAdmin database and commented out Roberts

--- databaseConnect.php
+++ databaseConnect.php
@@ -1,8 +1,13 @@
 <?php
     define('DB_HOST', 'mysql.cs.orst.edu');
-    define('DB_USER', 'cs340_rosenber');
-    define('DB_PASSWORD', '4321');
-    define('DB_NAME', 'cs340_rosenber');
+    //Robert's Database
+//    define('DB_USER', 'cs340_rosenber');
+//    define('DB_PASSWORD', '4321');
+//    define('DB_NAME', 'cs340_rosenber');
+    //Connor's Database
+    define('DB_USER', 'cs340_chriconn');
+    define('DB_PASSWORD', '4733');
+    define('DB_NAME', 'cs340_chriconn');
     
     $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
     
